588e165dc790f1fc2990ec3cb689b748561c61e1
Alexandr <alex-li.li2016@yandex.ru>
2023-08-24 17:13:03 +0600

    сделано!

--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -11,6 +11,15 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     archived = models.BooleanField(default=False)
 
+    # @property
+    # def description_short(self):
+    #     if len(self.description) < 48:
+    #         return self.description
+    #     return self.description[:48] + '...'
+
+    def __str__(self):
+        return f'Product(pk = {self.pk}, name = {self.name})'
+
 class Order(models.Model):
     delivery_address = models.TextField(null=True, blank=True)
     promocode = models.CharField(max_length=20, null=False, blank=True)
